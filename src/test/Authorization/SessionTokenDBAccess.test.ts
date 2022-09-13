6cd21e4c4f05560f9598f78eea8d86ec6ca348bd
Connie Rehfeld <crehfeld@marathonts.com>
2022-09-13 18:10:38 -0500

    Create SessionTokenDBAccess.test.ts
    Push to repository

---
+++ src/test/Authorization/SessionTokenDBAccess.test.ts
@@ -0,0 +1,7 @@
+import { SessionTokenDBAccess } from "../../app/Authorization/SessionTokenDBAccess";
+import * as Nedb from 'nedb';
+jest.mock('nedb');
+
+describe('SessionTokenDBAccess test suite', ()=> {
+let SessionTokenDBAccess: SessionTokenDBAccess;
+});
